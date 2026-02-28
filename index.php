183a729c6029cb52e454a9070ab099f6e472b481
Ahmad Shah Abdali Khan <148656937+ahmadkhano@users.noreply.github.com>
2026-02-28 15:13:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if (isset($_POST['contact_btn'])) {
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Ahmad</title>
+  <title>Ahmad Khan</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
   <!-- Favicons -->
